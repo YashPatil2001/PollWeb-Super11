e24f9312d5e88736e9459d4388ff642dd322a1d9
Dashrath <sandip9028162859@gmail.com>
2022-01-01 22:56:57 +0530

    basic setup(server, apis, schema)

--- projBackend/data_base/db.js
+++ projBackend/data_base/db.js
@@ -1,32 +1,29 @@
-const express=require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/pollDB");
 
-const pollSchema=new mongoose.Schema({
-   _id:{
-       type:Number,
-       required:true
-   },
-    _auth_name:{
-        type:String,
-        required:true
-    },
-    _catogery:{
-        type:String,
-        required:true
-    },
-    _question:{
-        type:String,
-        required:true
-    },
-    _option1:String,
-    _option2:String,
-    _option3:String,
-    _option4:String,
+const pollSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: true,
+  },
+  _auth_name: {
+    type: String,
+    required: true,
+  },
+  _catogery: {
+    type: String,
+    required: true,
+  },
+  _question: {
+    type: String,
+    required: true,
+  },
+  _option1: String,
+  _option2: String,
+  _option3: String,
+  _option4: String,
+});
+const Poll = mongoose.model("Poll", pollSchema);
 
-    })
-    const Poll=mongoose.model("Poll",pollSchema);
-
- 
-
- module.exports=Poll;
+module.exports = Poll;
